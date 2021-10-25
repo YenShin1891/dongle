1f356b9f8c0f18ce809d8ee4ab238187e3728c17
woanderer0 <woanderer@kaist.ac.kr>
2021-10-25 18:53:05 +0900

    Location permission, setting map center, default pin
    location permission with activityResult API
    setting map center to current location
    added experimental default pin in seoul station

--- EcoMap4/app/src/main/java/com/example/ecomap4/MapFragment.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/MapFragment.kt
@@ -1,11 +1,23 @@
 package com.example.ecomap4
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.ecomap4.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapView
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,10 +29,21 @@ class MapFragment : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+                if (isGranted){
+                    //I am happy
+                }else{
+                    //so sad
+                }
+            }
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         return binding.root
@@ -29,13 +52,33 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mapView = MapView(context)
+        val mapViewContainer = binding.mapView as ViewGroup
+        mapViewContainer.addView(mapView)
 
-//        binding.addPinButton.setOnClickListener {
-//            //Turn on camera
-//            val toast = Toast.makeText(this.context, "fragment button works!", Toast.LENGTH_SHORT)
-//            toast.show()
-//            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        //experimental pin at seoul station
+        val marker = MapPOIItem()
+        marker.itemName = "Default Marker"
+        marker.tag = 0
+        marker.mapPoint = MapPoint.mapPointWithGeoCoord(37.555923849029526, 126.97232388116235)
+
+        //need to designate two forms: not clicked, and when clicked. currently in default form
+        marker.markerType = MapPOIItem.MarkerType.BluePin
+        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+        mapView.addPOIItem(marker)
+
+        //move map center to current location if we have permission, else display toast message
+        binding.myLocationButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                val manager = requireContext().getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+                val location: Location? = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(location!!.latitude, location!!.longitude), false);
+            }
+            else{
+                val toast = Toast.makeText(this.context, "give me permission", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 
     override fun onDestroyView() {
