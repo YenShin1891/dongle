92d5122c667e657ef147eec5be132556420a4897
Jiheun <jhkaty20@kaist.ac.kr>
2021-10-31 02:11:38 +0900

    Android ID
    Get Android ID as UserID

--- EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.ecomap4
 
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val UserID= Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID) // context to this
     }
+
 }
