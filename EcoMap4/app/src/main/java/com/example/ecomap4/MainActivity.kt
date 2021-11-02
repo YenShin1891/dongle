a70eff8f02daa5d21cc03fb741cea48e2ebed39e
Jiheun <jhkaty20@kaist.ac.kr>
2021-11-03 05:04:08 +0900

    Download JPG from AWS
    Download succeeded but there is no file in android local directory

--- EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
@@ -1,14 +1,100 @@
 package com.example.ecomap4
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import androidx.core.content.FileProvider
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3Client
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val UserID= Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID) // context to this
+
+        val button = findViewById<Button>(R.id.sample_button)
+        button.setOnClickListener{
+            showWithTransferUtility()
+        }
+
     }
 
+
+    private fun showWithTransferUtility() {
+        // Initialize the Amazon Cognito credentials provider
+        val credentialsProvider = CognitoCachingCredentialsProvider(
+            applicationContext,
+            "ap-northeast-2:7f6c86c9-3c2a-41a5-93c3-a2e880bab0b2", // identity pool ID
+            Regions.AP_NORTHEAST_2 // Region
+        )
+
+        TransferNetworkLossHandler.getInstance(applicationContext)
+
+        val transferUtility = TransferUtility.builder()
+            .context(applicationContext)
+            .defaultBucket("dongle2021awsbuket")
+            .s3Client(AmazonS3Client(credentialsProvider, Region.getRegion(Regions.AP_NORTHEAST_2)))
+            .build()
+
+
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File.createTempFile(
+            "downloaded",".jpg",
+            storageDir
+        )
+
+
+        //val filePath = file.absolutePath
+
+        val photoURI: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.ecomap4.fileprovider", file
+        )
+        Log.d("URI", photoURI.toString())
+
+
+        val downloadObserver = transferUtility.download("dongle2021awsbuket", "main.JPG", file)
+
+        //val downloadObserver = transferUtility.download("main.JPG", File(filePath))
+
+        //val downloadObserver = transferUtility.download("main.JPG", File(storageDir!!.absolutePath + "/main.JPG"))
+
+
+
+        downloadObserver.setTransferListener(object : TransferListener {
+            override fun onStateChanged(id: Int, state: TransferState) {
+                if (state == TransferState.COMPLETED) {
+                    Log.d("AWS", "DOWNLOAD Completed!")
+                }
+            }
+
+            override fun onProgressChanged(id: Int, current: Long, total: Long) {
+                try {
+                    val done = (((current.toDouble() / total) * 100.0).toInt()) //as Int
+                    Log.d("AWS", "DOWNLOAD - - ID: $id, percent done = $done")
+                } catch (e: Exception) {
+                    Log.d("AWS", "Trouble calculating progress percent", e)
+                }
+            }
+
+            override fun onError(id: Int, ex: Exception) {
+                Log.d("AWS", "DOWNLOAD ERROR - - ID: $id - - EX: ${ex.message.toString()}")
+            }
+
+
+        })
+    }
 }
