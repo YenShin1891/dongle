132c3bd2e0223e768bba0b58e52c19b48c95f5e6
woanderer0 <woanderer@kaist.ac.kr>
2021-11-04 00:04:57 +0900

    Enabled locationUpdate using networkProvider
    along with GPSProvider

--- EcoMap4/app/src/main/java/com/example/ecomap4/MapFragment.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/MapFragment.kt
@@ -1,10 +1,14 @@
 package com.example.ecomap4
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -36,7 +40,8 @@ class MapFragment : Fragment() {
                 if (isGranted){
                     //I am happy
                 }else{
-                    //so sad
+                    val toast = Toast.makeText(context, "Denied permission: tracking service unavailable", Toast.LENGTH_SHORT)
+                    toast.show()
                 }
             }
             permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -67,22 +72,54 @@ class MapFragment : Fragment() {
         marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
         mapView.addPOIItem(marker)
 
+        //track current location
+        val manager = requireContext().getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+        var lastKnownLocation: Location? = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        val isGPSEnabled =
+            manager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (lastKnownLocation != null) {
+            Log.d("Test", "GPS Location changed, Latitude: ${lastKnownLocation.latitude}" +
+                    ", Longitude: ${lastKnownLocation.longitude}, GPSEnabled=${isGPSEnabled}")
+        }
+        else{
+            Log.d("Test", "somehow we don't have available gps location, GPSEnabled=${isGPSEnabled}")
+        }
+
+        var locationListener: LocationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                lastKnownLocation=location
+                Log.d("Test", "GPS Location changed, Latitude: ${location.latitude}" +
+                        ", Longitude: ${location.longitude}")
+
+            }
+            override fun onProviderEnabled(provider: String) {
+                Log.d("Test", "gpsListener is listening providerenabled")
+            }
+            override fun onProviderDisabled(provider: String) {
+                Log.d("Test", "gpsListener is listening providerdisabled")
+            }
+        }
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener);
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener);
+        }
+
         //move map center to current location if we have permission, else display toast message
         binding.myLocationButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                val manager = requireContext().getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
-                val location: Location? = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(location!!.latitude, location!!.longitude), false);
+                mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude), false);
             }
             else{
                 val toast = Toast.makeText(this.context, "give me permission", Toast.LENGTH_SHORT)
                 toast.show()
             }
         }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
