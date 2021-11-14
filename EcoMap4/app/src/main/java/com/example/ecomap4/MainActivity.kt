b362ef9b5b5f7e87436ab64e86e90823903ee8a0
woanderer0 <woanderer@kaist.ac.kr>
2021-11-15 03:04:10 +0900

    Listeners now work :)
    select a pin, and empty floating window appears.
    Deleted Mapfragment, and migrated all of its contents to mainActivity.
    Implemented LocationListener and POIItemEventListener in mainActivity

--- EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.ecomap4
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.Parcel
+import android.os.Parcelable
 import android.provider.Settings
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.amazonaws.auth.CognitoCachingCredentialsProvider
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
@@ -17,12 +23,26 @@ import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
+import com.example.ecomap4.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.navigation.NavigationView
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), MapView.POIItemEventListener, MapView.CurrentLocationEventListener{
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mapView: MapView
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<NavigationView>
+    private lateinit var currentLocation: MapPoint
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val UserID= Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID) // context to this
 
         val button = findViewById<Button>(R.id.sample_button)
@@ -30,6 +50,59 @@ class MainActivity : AppCompatActivity() {
             showWithTransferUtility()
         }
 
+
+        //------get location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+                if (isGranted){
+                    //I am happy
+                }else{
+                    val toast = Toast.makeText(this, "Denied permission: tracking service unavailable", Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+            }
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+
+
+        //======instantiating map related objects======
+        mapView = MapView(this)
+        binding.mapViewLayout.addView(mapView)
+        mapView.mapType=MapView.MapType.Hybrid
+        mapView.setPOIItemEventListener(this)
+        mapView.setCurrentLocationEventListener(this)
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeadingWithoutMapMoving
+        mapView.setShowCurrentLocationMarker(true)
+
+        //var currentLocation: MapPoint = MapPoint.mapPointWithGeoCoord(36.372884, 127.363503)
+        //mapView.setMapCenterPoint(currentLocation, false)
+
+        //Floating window to show when a pin is touched
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.navigationView)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        //Add POIItems
+        val firstMarker=addMapPOIItem("hehe", 36.372885, 127.363504)
+        val secondMarker=addMapPOIItem("hihi", 36.372800, 127.363599)
+        var markerList = mutableListOf(firstMarker, secondMarker)
+
+        //move map center to current location if we have permission, else display toast message
+        binding.myLocationButton.setOnClickListener {
+            bottomSheetBehavior.state=BottomSheetBehavior.STATE_HALF_EXPANDED
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                if (currentLocation != null){
+                    mapView.setMapCenterPoint(currentLocation, true)
+                }
+                else{
+                    val toast = Toast.makeText(this, "We don't know where you are, but we do have permission", Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+            }
+            else{
+                val toast = Toast.makeText(this, "give me permission", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 
 
@@ -97,4 +170,34 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
+
+    //======from below sang seong ei 's space======
+    private fun addMapPOIItem(idInput: String, lat: Double, lon: Double): MapPOIItem{
+        val pin = MapPOIItem()
+        pin.itemName=idInput//this property is essential for the pin to be visible. Let's store dbId here, and not use MMapPOIItem class
+        pin.mapPoint = MapPoint.mapPointWithGeoCoord(lat, lon)
+        pin.markerType = MapPOIItem.MarkerType.BluePin
+        pin.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+        mapView.addPOIItem(pin)
+        return pin
+    }
+
+    //Member methods of POIItemEventListener
+    override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+    }
+    override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {}
+    override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?, p2: MapPOIItem.CalloutBalloonButtonType?) {}
+    override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {}
+
+    //Member methods of CurrentLocationEventListener
+    override fun onCurrentLocationUpdate(mapView : MapView, currentLocationInListener : MapPoint, accuracyInMeters : Float){
+        currentLocation=currentLocationInListener
+    }
+    override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {}
+    override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+        Log.d("Location", "currentLocation update failed")
+    }
+    override fun onCurrentLocationUpdateCancelled(p0: MapView?) {}
 }
