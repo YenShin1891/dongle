14713c51dfb95339a5ddf5d6c49404b5c214ae80
Yeeun Shin <yeeun@kaist.ac.kr>
2021-12-11 22:42:54 +0900

    Changed app icon

--- EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
@@ -2,12 +2,7 @@ package com.example.ecomap4
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ecomap4.databinding.ActivityMainBinding
 import com.example.ecomap4.databinding.ActivityUploadBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.navigation.NavigationView
-import net.daum.mf.map.api.MapPoint
-import net.daum.mf.map.api.MapView
 
 class UploadActivity (): AppCompatActivity(){
 
