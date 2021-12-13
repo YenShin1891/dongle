39da4dc2bbfd1b1a90cf8c0fe3b585703f2fdb91
Yeeun Shin <yeeun@kaist.ac.kr>
2021-12-14 00:04:32 +0900

    Add Pin Page has TextFields

--- EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
@@ -1,7 +1,12 @@
 package com.example.ecomap4
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.example.ecomap4.databinding.ActivityUploadBinding
 
 class UploadActivity (): AppCompatActivity(){
@@ -12,6 +17,47 @@ class UploadActivity (): AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+        //    }
+
+
+        // Get input text
+        val input_location = binding.outlinedTextFieldLocation.editText?.text.toString()
+        val input_date = binding.outlinedTextFieldDate.editText?.text.toString()
+        val input_name = binding.outlinedTextFieldName.editText?.text.toString()
+        val input_memo = binding.outlinedTextFieldMemo.editText?.text.toString()
+
+//        if (input_date == null) {
+//            binding.tipResult.text = ""
+//            return
+//        }
+
+        binding.outlinedTextFieldLocation.editText?.doOnTextChanged { input_location, _, _, _ ->
+            // Respond to input text change
+        }
+        binding.outlinedTextFieldDate.editText?.doOnTextChanged { input_date, _, _, _ ->
+            // Respond to input text change
+        }
+        binding.outlinedTextFieldName.editText?.doOnTextChanged { input_name, _, _, _ ->
+            // Respond to input text change
+        }
+        binding.outlinedTextFieldMemo.editText?.doOnTextChanged { input_name, _, _, _ ->
+            // Respond to input text change
+        }
+
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
 }
