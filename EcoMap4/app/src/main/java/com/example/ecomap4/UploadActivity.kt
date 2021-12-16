f45ad828ea506bbd1aaf7910956ad99da3366cff
Yeeun Shin <yeeun@kaist.ac.kr>
2021-12-17 01:14:47 +0900

    Change calender text
    color and order

--- EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
+++ EcoMap4/app/src/main/java/com/example/ecomap4/UploadActivity.kt
@@ -44,11 +44,10 @@ class UploadActivity (): AppCompatActivity(){
                 calendar.time = Date(it)
                 val calendarMilli = calendar.timeInMillis
 
-                binding.DateInput.text = "${calendar.get(Calendar.MONTH)}/" +
-                        "${calendar.get(Calendar.DAY_OF_MONTH)}/" +
-                        "${calendar.get(Calendar.YEAR)}"
+                binding.DateInput.text = "${calendar.get(Calendar.YEAR)}/" +
+                        "${calendar.get(Calendar.MONTH)}/" +
+                        "${calendar.get(Calendar.DAY_OF_MONTH)}"
                 }
-
             datePicker.show(supportFragmentManager,datePicker.toString())
         }
 
